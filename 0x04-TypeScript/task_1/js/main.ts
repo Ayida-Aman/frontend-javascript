037dd8c4277f37fb322852fb6a822a9c0f90ae8c
Ayida-Aman <amanayida1@gmail.com>
2025-07-13 21:25:54 +0300

    created an instance that extends from teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,12 @@
 interface Teacher{
     readonly firstName: string;
     readonly lastName: string;
-    fullTimeEmployee: string;
-    yearsOfExperience?: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
     location: string;
     [key: string]:any;
+}
+
+interface Directors extends Teacher{
+    numberOfReports: number;
 }
